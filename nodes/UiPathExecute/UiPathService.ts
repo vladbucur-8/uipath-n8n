2b9864fa4eb1fd1f21d0492273e4013b2f17dce1
GabrielVasilescu04 <gabriel.vasilescu@uipath.com>
2025-06-04 14:28:20 +0300

    Add execute startJob

--- nodes/UiPathExecute/UiPathService.ts
+++ nodes/UiPathExecute/UiPathService.ts
@@ -1,5 +1,6 @@
 import { ILoadOptionsFunctions, NodeApiError, IHttpRequestMethods, INode } from 'n8n-workflow';
 
+
 interface UiPathCredentials {
     organization: string;
     tenant: string;
@@ -8,7 +9,7 @@ interface UiPathCredentials {
 
 export class UiPathService {
     private baseUrl: string;
-    private headers: { Authorization: string };
+    private headers: Record<string, string>;
     private helpers: ILoadOptionsFunctions['helpers'];
     private node: INode;
 
@@ -21,15 +22,16 @@ export class UiPathService {
         this.node = node;
     }
 
-    private async makeRequest(method: IHttpRequestMethods, url: string) {
+    private async makeRequest(method: IHttpRequestMethods, url: string, customRequestHeaders: Record<string, string> | null = null, body: object | null = null) {
         const response = await this.helpers.request({
             method,
             url: `${this.baseUrl}${url}`,
+            body: body,
             json: true,
-            headers: this.headers,
+            headers: customRequestHeaders == null ? this.headers : { ...this.headers, ...customRequestHeaders}
         });
 
-        if (!response || !response.value) {
+        if (!response) {
             throw new NodeApiError(this.node, { message: 'Unexpected API response format' });
         }
 
@@ -62,6 +64,47 @@ export class UiPathService {
         } catch (error) {
             console.error('Error fetching folders:', error.message);
             throw new NodeApiError(this.node, { message: 'Failed to fetch folders' });
+        }
+    }
+
+    async startJobAndWaitForFinalState(folderId: string, body: object) {
+
+        let maxRetries = 20;
+		let attempt = 0;
+        let requestHeaders: Record<string, string> = {
+            'X-UIPATH-OrganizationUnitId': `${folderId}`
+        }
+
+        try {
+            const startJobResponse = await this.makeRequest(
+                'POST',
+                '/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs',
+                requestHeaders,
+                body
+            )
+
+            const jobId = startJobResponse.value[0].Id;
+
+            while (attempt < maxRetries)
+			{
+				let getJobResponse = await this.makeRequest(
+                    'GET',
+                    `/odata/Jobs(${jobId})`,
+                    requestHeaders
+                )
+
+                if(getJobResponse.State == 'Successful' || getJobResponse.State == 'Faulted')
+				{
+					return getJobResponse;
+				}
+
+				await new Promise(resolve => setTimeout(resolve, 5000));
+				++attempt;
+			}
+        }
+        catch(error) {
+            console.error('Error starting job:', error.message);
+            throw new NodeApiError(this.node, {message: 'Failed to start Orchestrator job'});
         }
     }
 }
